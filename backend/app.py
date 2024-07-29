10ac0e2280718b1c1a8690efecc802d2ffaa9568
Abishanka Saha <abishanka.saha@gmail.com>
2024-07-29 15:34:27 -0400

    Compressed API routes

--- backend/app.py
+++ backend/app.py
@@ -7,62 +7,25 @@ def portfolio_overview():
     return jsonify()
 
 #Information on instruments currently in Portfolio
-@app.route('/portfolioinstrument/equity/<ticker>')
+@app.route('/portfolioinstrument/<string:instrumenttype>/<string:ticker>')
 def portfolio_equity(ticker):
     return jsonify()
 
-@app.route('/portfolioinstrument/bond/<ticker>')
-def portfolio_bond(ticker):
-    return jsonify()
-
-@app.route('/portfolioinstrument/cash/<ticker>')
-def portfolio_cash(ticker):
-    return jsonify()
-
 #Add new instrument to Portfolio
-@app.route('/addinstrument/equity/<ticker>')
+@app.route('/addinstrument/<string:instrumenttype>/<ticker>')
 def portfolio_add_equity(ticker):
     return jsonify()
 
-@app.route('/addinstrument/bond/<ticker>')
-def portfolio_add__bond(ticker):
-    return jsonify()
-
-@app.route('/addinstrument/cash/<ticker>')
-def portfolio_add__cash(ticker):
-    return jsonify()
-
 #Remove instrument from Portfolio
-@app.route('/delinstrument/equity/<ticker>')
+@app.route('/delinstrument/<string:instrumenttype>/<ticker>')
 def portfolio_del_equity(ticker):
     return jsonify()
 
-@app.route('/delinstrument/bond/<ticker>')
-def portfolio_del__bond(ticker):
-    return jsonify()
-
-@app.route('/delinstrument/cash/<ticker>')
-def portfolio_del__cash(ticker):
-    return jsonify()
-
 #Search for instrument
-@app.route('/searchinstrument/equity/', methods=['GET'])
+@app.route('/searchinstrument/<string:instrumenttype>/', methods=['GET'])
 def search_equity():
     query = request.args.get('query')
     return jsonify()
-
-@app.route('/searchinstrument/bond/', methods=['GET'])
-def search_bond():
-    query = request.args.get('query')
-    return jsonify()
-
-@app.route('/searchinstrument/cash/', methods=['GET'])
-def search_cash():
-    query = request.args.get('query')
-    return jsonify()
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
